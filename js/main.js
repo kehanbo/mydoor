8b4ad350ba4768ad2739cd4de6a42f4b1a17ac0f
kehanbo <875225214@qq.com>
2020-06-28 23:52:50 +0800

    rm -r -f .idea/

--- js/main.js
+++ js/main.js
@@ -24,4 +24,10 @@ $(function () {
 
 
 
+    $('#myTabs a').click(function (e) {
+        e.preventDefault()
+        $(this).tab('show');
+        $('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+    })
+
 });
